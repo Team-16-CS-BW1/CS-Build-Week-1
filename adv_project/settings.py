8659a869e964f11ec6750cfd1e1e47013bb33ead
Zurglots <bfeole@gmail.com>
2019-10-23 13:28:54 -0400

    Added dj database url import

--- adv_project/settings.py
+++ adv_project/settings.py
@@ -14,6 +14,7 @@ import django_heroku
 from rest_framework.authentication import SessionAuthentication, BasicAuthentication, TokenAuthentication
 import os
 from decouple import config
+import dj_database_url
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
